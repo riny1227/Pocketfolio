848d3598240ae2cc47eba9619e642f3f467dc638
Riny <add6847@naver.com>
2025-03-05 15:19:41 +0900

    비밀번호 찾기 & 로그인 navigate 수정

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -2,47 +2,53 @@ import { createContext, useContext, useState } from "react";
 import { login as loginApi } from "../api/LoginApi";
 import { logout as logoutApi } from "../api/LogoutApi";
 
-// AuthContext 생성
 const AuthContext = createContext();
 
 export function AuthProvider({ children }) {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
-    const [token, setToken] = useState(null); // 토큰 상태 추가
+    const [token, setToken] = useState(null);
 
     // 로그인 함수
-    const login = async (email, password) => {
+    const login = async (email, password, navigate) => {
         try {
-            // 로그인 API 호출
             const response = await loginApi(email, password);
 
             if (response.token) {
-                // 토큰 저장
                 setIsLoggedIn(true);
                 setToken(response.token);
                 console.log("로그인 성공:", response.message);
+                alert("로그인에 성공하였습니다.");
+
+                // 메인 페이지로 이동
+                navigate("/");
             }
         } catch (error) {
             console.error("로그인 실패:", error.message);
+
+            if (error.message === "비밀번호가 일치하지 않습니다.") {
+                alert("비밀번호가 일치하지 않습니다. 다시 확인해주세요.");
+            } else {
+                alert("로그인에 실패했습니다. 다시 시도해주세요.");
+            }
+
             throw new Error("로그인에 실패했습니다. 다시 시도해주세요.");
         }
     };
 
     // 로그아웃 함수
-    const logout = async () => {
+    const logout = async (navigate) => {
         try {
-            // 로그아웃 API 호출
             await logoutApi();
 
-            // 클라이언트에서 토큰 삭제
             setIsLoggedIn(false);
             setToken(null);
             console.log("로그아웃 성공");
 
-            // 로그인 페이지로 리다이렉트
-            window.location.href = "/login";
+            // 로그인 페이지로 이동
+            navigate("/login");
         } catch (error) {
             console.error("로그아웃 실패:", error.message);
-            throw new Error("로그아웃에 실패했습니다. 다시 시도해주세요.");
+            alert("로그아웃에 실패했습니다. 다시 시도해주세요.");
         }
     };
 
@@ -53,7 +59,6 @@ export function AuthProvider({ children }) {
     );
 }
 
-// useAuth 훅을 통해 context 사용
 export function useAuth() {
     return useContext(AuthContext);
 }
